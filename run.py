252224143b41399bb01137b9006b5cb0a3f5a567
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-18 12:11:34 +0100

    update

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__main__)
 
 @app.route("/")
-def home():
-    return "Hello! Main page <h1>HELLO<h1>1"
+def hello():
+    return ("Hello! Main page")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080') # indent this line
