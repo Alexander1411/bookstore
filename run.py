ae0b2ef8800e2fc23550da26e28c52b53fff71a4
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-18 12:17:52 +0100

    typo

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__main__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return ("Hello! Main page")
+    return "Hello! Main page"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080') # indent this line
