0ccfc920372b3e768d015d3106b10b33616a0d79
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 20:19:42 +0100

    127.0.0.1

--- run.py
+++ run.py
@@ -116,4 +116,4 @@ def cart():
     return render_template('cart.html', books=cart_books, quantities=cart_quantities)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
