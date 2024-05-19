aefb6d6a7a4c6bfdb6ff7ad81c0e763de4b20837
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 14:22:37 +0100

    update

--- run.py
+++ run.py
@@ -14,5 +14,10 @@ def home():
 def shop():
     return render_template("shop.html")
 
+@app.route("/search")
+def search():
+    query = request.args.get('query')
+    return f"Search results for: {query}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
