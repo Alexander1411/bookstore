a1acb7b24bca39fc736cfda8e438653032ed5e33
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 13:27:58 +0100

    fixing typo

--- run.py
+++ run.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
- return "<h1>Hello World but bigger</h1>!" #indent this line
-
 @app.route("/")
-@app.route('/home')
+def hello():
+    return "<h1>Hello World but bigger</h1>"
+
+@app.route("/home")
 def home():
-    return render_template(home.html)
- 
-if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080') # indent this line
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
