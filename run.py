6e3e0c522b6dce6d5fe0d53881435a585b64d498
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 13:05:14 +0100

    test

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
- return("Hello World!") #indent this line
+ return "<h1>Hello World but bigger</h1>!" #indent this line
  
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
