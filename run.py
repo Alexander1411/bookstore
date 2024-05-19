5988dcbe4fca3d99eeb1d13586174673e9516487
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 13:12:07 +0100

    add about

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")#URL leading to method
 def hello(): # Name of the method
  return "<h1>Hello World but bigger</h1>!" #indent this line
+
+@app.route("/about/<username>")
+def about_page(username):
+    return f"<h1>This is about page of {username}</h1>"
  
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') # indent this line
