864216a9e0362cbdbf7f76cf94d0a1755d1bb3cd
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-19 14:41:25 +0100

    big update

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+
+books = [
+    {"id": 1, "title": "Book One", "author": "Author One"},
+    {"id": 2, "title": "Book Two", "author": "Author Two"},
+    {"id": 3, "title": "Book Three", "author": "Author Three"},
+    {"id": 4, "title": "Book Four", "author": "Author Four"},
+    {"id": 5, "title": "Book Five", "author": "Author Five"},
+    {"id": 6, "title": "Book Six", "author": "Author Six"}
+]
 
 @app.route('/')
 def home():
@@ -9,25 +18,26 @@ def home():
 
 @app.route('/login')
 def login():
-    # Add login logic here
     session['user'] = 'user'
     return redirect(url_for('home'))
 
 @app.route('/logout')
 def logout():
-    # Add logout logic here
     session.pop('user', None)
     return redirect(url_for('home'))
 
 @app.route('/books')
-def books():
-    # Add logic to show books
-    return render_template('books.html')
+def books_page():
+    query = request.args.get('query')
+    if query:
+        filtered_books = [book for book in books if query.lower() in book['title'].lower()]
+    else:
+        filtered_books = books
+    return render_template('books.html', books=filtered_books)
 
 @app.route('/cart')
 def cart():
-    # Add logic to show cart
     return render_template('cart.html')
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
