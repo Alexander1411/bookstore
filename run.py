011e79d8500b2f84537f0ae5949ca62a3fd61c0d
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-20 17:59:24 +0100

    app.config["MYSQL_PASSWORD"] = "alexander"

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app.secret_key = 'supersecretkey'
 # MySQL Configuration
 app.config["MYSQL_HOST"] = "10.0.0.4" #my vm ip
 app.config["MYSQL_USER"] = "remote_user"
-app.config["MYSQL_PASSWORD"] = "yourpassword"
+app.config["MYSQL_PASSWORD"] = "alexander"
 app.config["MYSQL_DB"] = "bookstore_users"
 
 
