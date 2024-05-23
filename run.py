95b7963a1a7d7bed4b531af315b605d6d910337f
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-23 17:17:52 +0100

    Removed a duplicate - return render_template('edit_profile.html', user=user)

--- run.py
+++ run.py
@@ -226,8 +226,5 @@ def edit_profile():
     
     return render_template('edit_profile.html', user=user)
 
-    
-    return render_template('edit_profile.html', user=user)
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
